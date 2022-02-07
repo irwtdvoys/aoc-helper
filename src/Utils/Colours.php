668a7868f11633f78f01009e83cc5305e6252b89
Steve Adams <irwtdvoys@gmail.com>
2022-02-07 12:17:13 +0000

    Add format method with array of options including new styling

--- src/Utils/Colours.php
+++ src/Utils/Colours.php
@@ -12,10 +12,27 @@
 		const CYAN = "36";
 		const WHITE = "37";
 
+		const BG_BLACK = "40";
+		const BG_RED = "41";
+		const BG_GREEN = "42";
+		const BG_YELLOW = "43";
+		const BG_BLUE = "44";
+		const BG_MAGENTA = "45";
+		const BG_CYAN = "46";
+		const BG_WHITE = "47";
+
 		const RESET = "0;0";
 
 		const TYPE_STANDARD = "0";
 		const TYPE_BOLD = "1";
+		const TYPE_ITALIC = "3";
+		const TYPE_UNDERLINE = "4";
+		const TYPE_STRIKETHROUGH = "9";
+
+		public static function format(array $format, string $string): string
+		{
+			return "\e[" . implode(";", $format) . "m" . $string . "\e[" . self::RESET . "m";
+		}
 
 		public static function colour(string $string, string $colour = self::RESET, string $type = self::TYPE_STANDARD)
 		{
